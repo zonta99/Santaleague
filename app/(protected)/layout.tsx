ada385a7aa98c8c9da13bc762457d47bb19fc307
Riccardo <zonta.riccardo99@gmail.com>
2024-03-26 21:27:17 +0100

    ADD FOOTER

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -1,4 +1,5 @@
 import { Navbar } from "./_components/navbar";
+import {MobileNavbar} from "@/components/component/mobile-navbar";
 
 interface ProtectedLayoutProps {
   children: React.ReactNode;
@@ -6,9 +7,10 @@ interface ProtectedLayoutProps {
 
 const ProtectedLayout = ({ children }: ProtectedLayoutProps) => {
   return ( 
-    <div className="h-full w-full flex flex-col gap-y-10 items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+    <div className="overflow-hidden h-screen w-screen flex flex-col gap-y-10 items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
       <Navbar />
       {children}
+        <MobileNavbar/>
     </div>
    );
 }
